adf67b4002cbecdd243cb772d82b8ec71ec2eb6b
UsuarioDeCreme <leosoares.me2@gmail.com>
2024-09-24 17:42:19 -0300

    s

--- src/pages/home/components/products/productList/index.tsx
+++ src/pages/home/components/products/productList/index.tsx
@@ -1,46 +1,46 @@
-import i18next, { t } from "i18next";
-import { ProductsType, Props } from "../../../types";
+// import i18next, { t } from "i18next";
+// import { ProductsType, Props } from "../../../types";
 
-export const ProductList = () => {
-  const sign = i18next.language === "br" ? "R$" : "$";
+// export const ProductList = () => {
+//   const sign = i18next.language === "br" ? "R$" : "$";
 
-  const promotions: ProductsType[] = [
-    {
-      id: 1,
-      name: "Celular novo 2024, 16gb ram processador ultima geração tela ips aaaaaaaaaaaaaaa aaaaaaaa aaaaaaaaa",
-      price: 50,
-      discount: 20,
-      userEmail: "",
-      userName: "",
-      stock: 10,
-      image_url: "",
-      rating: 3.2,
-      ratingLength: 30,
-      purchased: 2,
-      freight: 20,
-      description: "",
-    },
-  ];
-  const productsComponents: Props[] = [
-    {
-      title: t("bestSellers"),
-      currencySign: sign,
-      products: promotions,
-    },
-    {
-      title: t("titlePromotion"),
-      currencySign: sign,
-      products: promotions,
-    },
-    {
-      title: t("title"),
-      currencySign: sign,
-      products: promotions,
-    },
-  ];
+//   const promotions: ProductsType[] = [
+//     {
+//       id: 1,
+//       name: "Celular novo 2024, 16gb ram processador ultima geração tela ips aaaaaaaaaaaaaaa aaaaaaaa aaaaaaaaa",
+//       price: 50,
+//       discount: 20,
+//       userEmail: "",
+//       userName: "",
+//       stock: 10,
+//       image_url: "",
+//       rating: 3.2,
+//       ratingLength: 30,
+//       purchased: 2,
+//       freight: 20,
+//       description: "",
+//     },
+//   ];
+//   const productsComponents: Props[] = [
+//     {
+//       title: t("bestSellers"),
+//       currencySign: sign,
+//       products: promotions,
+//     },
+//     {
+//       title: t("titlePromotion"),
+//       currencySign: sign,
+//       products: promotions,
+//     },
+//     {
+//       title: t("title"),
+//       currencySign: sign,
+//       products: promotions,
+//     },
+//   ];
 
-  return (
-    <>
-    </>
-  )
-};
+//   return (
+//     <>
+//     </>
+//   )
+// };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,32 +1,32 @@
-import { Hero } from "./components/Hero";
-import { ProductionHome } from "./components/products";
-import { useTranslation } from "react-i18next";
+// import { Hero } from "./components/Hero";
+// import { ProductionHome } from "./components/products";
+// import { useTranslation } from "react-i18next";
 
-import i18next from "i18next";
-import { productsComponents } from "./components/products/productList";
+// import i18next from "i18next";
+// import { productsComponents } from "./components/products/productList";
 
-export const Home = () => {
-  const { t } = useTranslation();
+// export const Home = () => {
+//   const { t } = useTranslation();
   
 
-  return (
-    <main className="relative z-10">
-      <section>
-        <Hero
-          heroText={t("heroText")}
-          language={i18next.language}
-          heroButton={t("heroButton")}
-        />
-      </section>
-      {productsComponents.map((comp) => (
-        <section key={comp.title} className="w-full mt-[80px]">
-          <ProductionHome
-            title={comp.title}
-            currencySign={comp.currencySign}
-            products={comp.products}
-          />
-        </section>
-      ))}
-    </main>
-  );
-};
+//   return (
+//     <main className="relative z-10">
+//       <section>
+//         <Hero
+//           heroText={t("heroText")}
+//           language={i18next.language}
+//           heroButton={t("heroButton")}
+//         />
+//       </section>
+//       {productsComponents.map((comp) => (
+//         <section key={comp.title} className="w-full mt-[80px]">
+//           <ProductionHome
+//             title={comp.title}
+//             currencySign={comp.currencySign}
+//             products={comp.products}
+//           />
+//         </section>
+//       ))}
+//     </main>
+//   );
+// };
